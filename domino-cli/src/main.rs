27901964780a488fbbff5a1c8d877c97f4aa59f8
jyuch <jyuch@users.noreply.github.com>
2023-11-04 22:02:21 +0900

    gRPC

--- domino-cli/src/main.rs
+++ domino-cli/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod hello_world {
+    tonic::include_proto!("hello_world");
+}
+
+use crate::hello_world::hello_service_client::HelloServiceClient;
+use crate::hello_world::HelloRequest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = HelloServiceClient::connect("http://127.0.0.1:50051").await?;
+    let request = tonic::Request::new(HelloRequest {
+        name: String::from("domino"),
+    });
+    let response = client.say_hello(request).await?;
+    println!("{}", response.into_inner().message);
+
+    Ok(())
 }
--- domino/build.rs
+++ domino/build.rs
@@ -1,10 +1,21 @@
-fn main() -> std::io::Result<()> {
+use std::env;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(windows)]
     {
         let mut res = winres::WindowsResource::new();
         res.set_icon("../media/icon.ico");
         res.compile()?;
     }
+
+    tonic_build::configure()
+        .build_server(true)
+        .file_descriptor_set_path(PathBuf::from(env::var("OUT_DIR")?).join("descriptor.bin"))
+        .compile(
+            &["../proto/hello_world/hello_world.proto"],
+            &["../proto/hello_world"],
+        )?;
 
     Ok(())
 }
